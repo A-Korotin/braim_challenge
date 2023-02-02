7631e48821d13a419e07238c4219768aff9766f6
Alexey Korotin <sx12fx@gmail.com>
2023-02-02 19:17:50 +0300

    Initial commit

---
+++ src/main/java/org/simbir_soft/braim_challenge/BraimChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.simbir_soft.braim_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraimChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraimChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/simbir_soft/braim_challenge/BraimChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simbir_soft.braim_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraimChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
