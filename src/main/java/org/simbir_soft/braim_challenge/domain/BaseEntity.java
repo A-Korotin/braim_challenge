6f7894db50a8082e250eaf9bfd048d6bbb30873c
Alexey Korotin <sx12fx@gmail.com>
2023-02-03 13:53:55 +0300

    Identity ID generation strategy

--- src/main/java/org/simbir_soft/braim_challenge/domain/BaseEntity.java
+++ src/main/java/org/simbir_soft/braim_challenge/domain/BaseEntity.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 public abstract class BaseEntity implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
     private Long id;
 }
