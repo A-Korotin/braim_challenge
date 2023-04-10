c52a64d769921b2df83ad69868d8bc94d04cc4c9
alexey <sx12fx@gmail.com>
2023-04-10 16:13:50 +0300

    Fix: polygon validation

--- src/main/java/org/simbir_soft/braim_challenge/validation/internal/Line.java
+++ src/main/java/org/simbir_soft/braim_challenge/validation/internal/Line.java
@@ -9,7 +9,9 @@ public class Line {
 
     public boolean pointOnLine(Point p) {
         // уравнение прямой, проходящей через 2 точки
-        return (p1.getY() - p2.getY()) * p.getX() + (p2.getX() - p1.getX()) * p.getY()
-                - (p1.getX() * p2.getY() - p2.getX() * p1.getY()) < THRESHOLD;
+        double equationResult = (p1.getY() - p2.getY()) * p.getX() + (p2.getX() - p1.getX()) * p.getY()
+                + (p1.getX() * p2.getY() - p2.getX() * p1.getY());
+
+        return Math.abs(equationResult) < THRESHOLD;
     }
 }
