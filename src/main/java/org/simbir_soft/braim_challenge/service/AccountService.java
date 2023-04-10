fd4ce173588e328d779b14dde630a7cef77edf56
alexey <sx12fx@gmail.com>
2023-04-10 16:07:38 +0300

    Internal: account loading

--- src/main/java/org/simbir_soft/braim_challenge/service/AccountService.java
+++ src/main/java/org/simbir_soft/braim_challenge/service/AccountService.java
@@ -2,11 +2,12 @@ package org.simbir_soft.braim_challenge.service;
 
 import org.simbir_soft.braim_challenge.domain.Account;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface AccountService extends CrudService<Account, Long> {
     Optional<Account> findByEmail(String email);
 
     Account register(Account account);
+
+    Optional<Account> findByIdInternal(Long id);
 }
