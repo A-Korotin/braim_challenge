94364b8c84224fd38d51baf12d9a69673f9e2952
alexey <sx12fx@gmail.com>
2023-04-10 16:14:08 +0300

    Feature: Area controller

--- src/main/java/org/simbir_soft/braim_challenge/controller/AreaController.java
+++ src/main/java/org/simbir_soft/braim_challenge/controller/AreaController.java
@@ -1,23 +1,29 @@
 package org.simbir_soft.braim_challenge.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-import org.simbir_soft.braim_challenge.domain.Area;
 import org.simbir_soft.braim_challenge.domain.dto.AreaDto;
+import org.simbir_soft.braim_challenge.exception.DataMissingException;
+import org.simbir_soft.braim_challenge.service.AreaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Validated
 public class AreaController {
 
+    private final AreaService areaService;
+
     @PostMapping("/areas")
     public ResponseEntity<?> createArea(@Valid @RequestBody AreaDto dto) {
-        Area area = dto.fromDto();
-        return ResponseEntity.ok(area);
+        return ResponseEntity.status(201).body(areaService.save(dto));
+    }
+
+    @GetMapping("/areas/{areaId}")
+    public ResponseEntity<?> getArea(@PathVariable @Min(1) Long areaId) {
+        return ResponseEntity.ok(areaService.findById(areaId).orElseThrow(DataMissingException::new));
     }
 }
