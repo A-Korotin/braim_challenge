9ad7b0a9cfd5ea4a28f990a068d4be41f5d7b99a
alexey <sx12fx@gmail.com>
2023-04-18 14:41:05 +0300

    Feature: secret endpoints done

--- src/main/java/org/simbir_soft/braim_challenge/controller/LocationController.java
+++ src/main/java/org/simbir_soft/braim_challenge/controller/LocationController.java
@@ -14,6 +14,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.Base64;
 
 @RestController
@@ -78,7 +80,9 @@ public class LocationController {
                                      @DecimalMin("-180")
                                      @DecimalMax("180")
                                      BigDecimal longitude) {
-        return ResponseEntity.ok(GeoHash.geoHashStringWithCharacterPrecision(latitude.doubleValue(), longitude.doubleValue(), 16));
+        String hash = GeoHash.withCharacterPrecision(latitude.doubleValue(), longitude.doubleValue(), 12).toBase32();
+        byte[] bytes = hash.getBytes(StandardCharsets.UTF_8);
+        return ResponseEntity.ok(new String(Base64.getEncoder().encode(bytes)));
     }
     @GetMapping("/geohashv3")
     public ResponseEntity<?> getHashv3(@RequestParam
@@ -88,9 +92,19 @@ public class LocationController {
                                      @RequestParam
                                      @DecimalMin("-180")
                                      @DecimalMax("180")
-                                     BigDecimal longitude) {
-        String hash = GeoHash.geoHashStringWithCharacterPrecision(latitude.doubleValue(), longitude.doubleValue(), 12);
+                                     BigDecimal longitude) throws Throwable {
+        String hash = GeoHash.withCharacterPrecision(latitude.doubleValue(), longitude.doubleValue(), 12).toBase32();
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] encoded = md5.digest(hash.getBytes(StandardCharsets.UTF_8));
+        int i = encoded.length - 1, j = 0;
+        while (i > j) {
+            byte tmp = encoded[i];
+            encoded[i] = encoded[j];
+            encoded[j] = tmp;
+            i--;
+            j++;
+        }
 
-        return ResponseEntity.ok(Base64.getEncoder().encode(hash.getBytes()));
+        return ResponseEntity.ok(Base64.getEncoder().encode(encoded));
     }
 }
